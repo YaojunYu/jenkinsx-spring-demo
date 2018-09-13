8400933574fc6b23675f1bb7680c862d82b431f1
yaojun <yojun.yu@gmail.com>
2018-09-13 18:54:05 +0800

    Initial import

---
+++ src/main/java/com/junchen/jenkinsxspringdemo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.junchen.jenkinsxspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
